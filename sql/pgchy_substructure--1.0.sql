0f0918094191b4c6915d3cdba11af48d1458d034
dzanadvo <dzanadvo@its.jnj.com>
2024-11-08 19:48:14 +0000

    fix

--- sql/pgchy_substructure--1.0.sql
+++ sql/pgchy_substructure--1.0.sql
@@ -13,17 +13,15 @@ language plpython3u
 as $function$
 import chython
 from sys import byteorder
-try:
-    query = chython.smarts(smarts)
-    if query.connected_components_count != 1:
-        return None
-    package = []
-    for x in query._cython_compiled_query[0]:
-        package.append((x.itemsize * len(x) + 2).to_bytes(2, byteorder))
-        package.append(x)
-    return b''.join(package)
-except Exception as e:
+query = chython.smarts(smarts)
+if query.connected_components_count != 1:
     return None
+package = []
+for x in query._cython_compiled_query[0]:
+    package.append((x.itemsize * len(x) + 2).to_bytes(2, byteorder))
+    package.append(x)
+return b''.join(package)
+
 $function$;
 
 
@@ -34,18 +32,14 @@ language plpython3u
 as $function$
 import chython
 from sys import byteorder
-try:
-    mol_container = chython.smiles(smiles)
-    package = []
-    for x in mol_container._cython_compiled_structure:
-        package.append((x.itemsize * len(x) + 2).to_bytes(2, byteorder))
-        print((x.itemsize * len(x) + 2))
-        package.append(x)
-    x = array('I', [1 for _ in mol_container])
+mol_container = chython.smiles(smiles)
+package = []
+for x in mol_container._cython_compiled_structure:
     package.append((x.itemsize * len(x) + 2).to_bytes(2, byteorder))
     package.append(x)
-    return b''.join(package)
+x = array('I', [1 for _ in mol_container])
+package.append((x.itemsize * len(x) + 2).to_bytes(2, byteorder))
+package.append(x)
+return b''.join(package)
 
-except Exception as e:
-    return None
 $function$;
